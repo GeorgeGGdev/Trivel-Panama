208c6d00f05c6aea243d78d2fb6f9f6c28f62860
MelanyProgramming <arauzmelany0@gmail.com>
2025-06-12 16:04:55 -0500

    agrege los comentarios

--- public/JS/comments.js
+++ public/JS/comments.js
@@ -1,28 +1,22 @@
 let currentIndex = 0;
-const testimonials = document.querySelectorAll('.testimonial');
+const testimonials = document.querySelectorAll('.carousel-item');
+const totalTestimonials = testimonials.length;
 
-function showTestimonial(index) {
-    if (index >= testimonials.length) {
+function moveCarousel(direction) {
+    testimonials[currentIndex].classList.remove('active');
+    
+    currentIndex += direction;
+    
+    if (currentIndex < 0) {
+        currentIndex = totalTestimonials - 1;
+    } else if (currentIndex >= totalTestimonials) {
         currentIndex = 0;
-    } else if (index < 0) {
-        currentIndex = testimonials.length - 1;
-    } else {
-        currentIndex = index;
     }
-
-    // Ocultar todos los testimonios
-    testimonials.forEach(testimonial => {
-        testimonial.style.display = 'none';
-    });
-
-    // Mostrar el testimonio actual
-    testimonials[currentIndex].style.display = 'block';
+    
+    testimonials[currentIndex].classList.add('active');
 }
 
-// Iniciar el carrusel mostrando el primer testimonio
-showTestimonial(currentIndex);
-
-// Funciones para cambiar de testimonio
-function changeTestimonial(direction) {
-    showTestimonial(currentIndex + direction);
-}
+// Automatizar el carrusel para que cambie cada 5 segundos
+setInterval(() => {
+    moveCarousel(1);
+}, 5000);
